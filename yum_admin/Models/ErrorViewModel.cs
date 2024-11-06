a3bac43349322c772d80d2e19593c81d70834f85
Chris Chou <tsunghaochou@gmail.com>
2024-11-06 17:48:33 +0800

    feat: add initial admin project

---
+++ yum_admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace yum_admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ yum_admin/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(options =>
+{
+    // 可以新增多個來源
+    options.AddPolicy("LiveServer",
+        policy =>
+        {
+            policy.WithOrigins("http://127.0.0.1:5500")  // 不要加斜線。
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+        });
+});
+
+
+// EF  等DBCONTEXT命名出來就可以引用了。 
+//builder.Services.AddDbContext<YourDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+        options.JsonSerializerOptions.Encoder =
+            JavaScriptEncoder.Create(
+                UnicodeRanges.BasicLatin,
+                UnicodeRanges.CjkUnifiedIdeographs
+            );
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseCors();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
