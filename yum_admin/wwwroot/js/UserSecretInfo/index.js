ae200b9f82c5d6a790d7c25a5af27ea712abd5ba
Chris Chou <tsunghaochou@gmail.com>
2024-11-20 17:22:34 +0800

    feat: add js file

---
+++ yum_admin/wwwroot/js/UserSecretInfo/index.js
@@ -0,0 +1,14 @@
+﻿$('.password-cell').each(function () {
+    // Get the actual password
+    password = $(this).text().trim();
+    console.log(password);
+    console.log(password.length);
+
+    // Replace the text with bullet points
+    var maskedPassword = '•'.repeat(password.length);
+    console.log(maskedPassword);
+
+
+    // Set the cell's content to the masked password
+    $(this).text(maskedPassword);
+});
