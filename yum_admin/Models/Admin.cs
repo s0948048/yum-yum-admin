adaa6e7a415a6304b8d1ddea84fb2fdfdc7a58cb
FangTzu-Chiang <onion110324@gmail.com>
2024-12-02 16:35:10 +0800

    feat:admin model css

--- yum_admin/Models/Admin.cs
+++ yum_admin/Models/Admin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace yum_admin.Models;
 
@@ -7,15 +8,21 @@ public partial class Admin
 {
     public int AdminId { get; set; }
 
+    [Display(Name = "帳號")]
     public string AdminAccount { get; set; } = null!;
 
+    [Display(Name = "密碼")]
     public string AdminPassword { get; set; } = null!;
 
+    [Display(Name = "名字")]
     public string AdminName { get; set; } = null!;
 
+    [Display(Name = "Email")]
     public string AdminEmail { get; set; } = null!;
 
+    [Display(Name = "頭像")]
     public string? AdminHeadShot { get; set; }
 
+    [Display(Name = "手機")]
     public string AdminPhone { get; set; } = null!;
 }
